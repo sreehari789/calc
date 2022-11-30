4664d355040c234f1682db67b33150c850aabec0
sreehari789 <sreeharipraj3693@gmail.com>
2022-11-30 16:40:06 +0530

    calculator v1

---
+++ calc.js
@@ -0,0 +1,14 @@
+function display (num){
+    result.value += num
+}
+
+function allClr() {
+result.value=" "
+}
+
+function eql(){
+    result.value=eval(result.value)
+}
+function back(){
+result.value=result.value.slice(0,-1)
+}
